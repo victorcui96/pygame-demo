ce4b72deeec2d92b80a462e384b50436db5ee5bc
Victor Cui <victor.cui1996@gmail.com>
2018-07-02 10:03:37 -0400

    First commit.

---
+++ game.py
@@ -0,0 +1,16 @@
+import pygame, sys
+from pygame.locals import *
+
+# set up pygame
+pygame.init()
+
+
+# set up the window
+
+
+windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption("Hello world!")
+
+
+
+
