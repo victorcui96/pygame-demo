521afaa703f23ab35506b570c90d12af3bdad524
Victor Cui <victor.cui1996@gmail.com>
2018-07-02 11:04:06 -0400

    Show how to fill the background with white.

--- game.py
+++ game.py
@@ -4,14 +4,16 @@ from pygame.locals import *
 # set up pygame
 pygame.init()
 
+white = [255, 255, 255] #RGB -> Use RGB because computers have a backlight 
+black = [0, 0, 0]
+red = [255, 0, 0]
 
 # set up the window
 
 # tuple = (x, y) where x = width of surface in pixels, y = height of surface in pixels 
-windowSurface = pygame.display.set_mode((500, 400))
+windowSurface = pygame.display.set_mode((500, 400)) # returns a pygame surface object
 pygame.display.set_caption("Slither")
 # Updates entire surface
-pygame.display.update()
 
 gameExit = False
 while not gameExit:
@@ -20,6 +22,11 @@ while not gameExit:
 		# print(event)
 		if event.type == pygame.QUIT:
 			gameExit = True
+	windowSurface.fill(white)
+	pygame.display.update() # rendering the graphic is the most CPU intensive 
+
+
+# TODO: Add another condition for where user loses the game, don't want to exit game entirely. 
 		
 
 
