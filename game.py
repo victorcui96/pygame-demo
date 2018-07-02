4bfa44bb73e50f9674eca419c12d847b641ebb2f
Victor Cui <victor.cui1996@gmail.com>
2018-07-02 11:16:50 -0400

    make the rectangle move.

--- game.py
+++ game.py
@@ -14,7 +14,8 @@ red = [255, 0, 0]
 windowSurface = pygame.display.set_mode((500, 400)) # returns a pygame surface object
 pygame.display.set_caption("Slither")
 # Updates entire surface
-
+leadX = 200
+leadY = 200
 gameExit = False
 while not gameExit:
 	# Pygame knows the events to watch out for (not on onus of programmer). It's up to you to do the event handling
@@ -22,7 +23,13 @@ while not gameExit:
 		# print(event)
 		if event.type == pygame.QUIT:
 			gameExit = True
-	windowSurface.fill(white)
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				leadX -= 10
+			if event.key == pygame.K_RIGHT:
+				leadX += 10
+	windowSurface.fill(white) #cleans the slate
+	pygame.draw.rect(windowSurface, red, [leadX, leadY, 50, 50]) # parameters = where we want to draw it, the color, and the coordinates
 	pygame.display.update() # rendering the graphic is the most CPU intensive 
 
 
