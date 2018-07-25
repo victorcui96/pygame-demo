45194248f88e4181580f2ce601f6d28c799f0a3a
Victor Cui <victor.cui1996@gmail.com>
2018-07-25 09:57:07 -0400

    Add wall checking.

--- game.py
+++ game.py
@@ -10,7 +10,9 @@ red = [255, 0, 0]
 
 gameExit = False
 # set up the window, tuple = (x, y) where x = width of surface in pixels, y = height of surface in pixels 
-windowSurface = pygame.display.set_mode((500, 400)) # returns a pygame surface object
+gameWidth = 500
+gameHeight = 400
+windowSurface = pygame.display.set_mode((gameWidth, gameHeight)) # returns a pygame surface object
 pygame.display.set_caption("Slither")
 # Updates entire surface
 leadX = 200 # Changing this is better than changing FPS -> explain why
@@ -29,24 +31,26 @@ while not gameExit:
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
 				leadXChange -= 10
-			if event.key == pygame.K_RIGHT:
+			elif event.key == pygame.K_RIGHT:
 				leadXChange += 10
-			if event.key == pygame.K_UP:
+			elif event.key == pygame.K_UP:
 				leadYChange -= 10
-			if event.key == pygame.K_DOWN:
+			elif event.key == pygame.K_DOWN:
 				leadYChange += 10
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
 				leadXChange = 0
 			elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
 				leadYChange = 0
+	if leadX >= gameWidth or leadX < 0 or leadY < 0 or leadY > gameHeight:
+		gameExit = True
 	leadX += leadXChange
 	leadY += leadYChange
 	windowSurface.fill(white) #cleans the slate
 	pygame.draw.rect(windowSurface, black, [leadX, leadY, 10, 10]) # parameters = where we want to draw it, the color, and the coordinates
 	pygame.display.update() # rendering the graphic is the most CPU intensive 
 	# specify FPS
-	clock.tick(10)
+	clock.tick(20)
 
 # TODO: Add another condition for where user loses the game, don't want to exit game entirely. 
 		
